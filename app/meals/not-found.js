ad17706c7609865372928a75b58b7542c606ee0b
Mohsin Tabba Ovais <ahme.mohsin@gmail.com>
2024-09-11 14:48:13 +0500

    Food app Next.js project initialized

---
+++ app/meals/not-found.js
@@ -0,0 +1,11 @@
+'use client';
+
+export default function NotFound({}) {
+  return (
+    <main className="not-found">
+      <h1>Meals not found</h1>
+
+      <p> Unfortunately, we could not find the requested page or meal data.</p>
+    </main>
+  );
+}
---
+++ components/meals/image-picker.js
@@ -0,0 +1,57 @@
+'use client';
+import { useRef, useState, useEffect } from 'react';
+import Image from 'next/image';
+import classes from './image-picker.module.css';
+
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState(null);
+  const imageInput = useRef();
+
+  function handlePickClick() {
+    imageInput.current.click();
+  }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      setPickedImage(reader.result);
+    };
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image src={pickedImage} alt="Image selected by the User" fill />
+          )}
+        </div>
+        <input
+          className={classes.input}
+          type="file"
+          id="image"
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInput}
+          onChange={handleImageChange}
+          required
+        />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}>
+          Pick and Image
+        </button>
+      </div>
+    </div>
+  );
+}
